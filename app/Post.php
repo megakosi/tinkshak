6102b2822724adf9620c0d0ba909379d1ed96f86
KosiEric <itskosieric@gmail.com>
2019-08-04 18:56:32 +0100

    done with "posts" database migrations

---
+++ app/Post.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    //
+
+
+}
--- database/migrations/2019_08_04_175115_create_admins_table.php
+++ database/migrations/2019_08_04_175115_create_admins_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAdminsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('email_verified_at')->default(now());
+            $table->bigInteger('number_of_posts')->default(0);
+            $table->timestamp('last_login_timestamp')->default(now());
+            $table->timestamp('last_post_timestamp')->default(now());
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
@@ -31,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admins');
     }
 }
